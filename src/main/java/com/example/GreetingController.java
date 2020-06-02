9bda76e6d88192a54b6a28dc0935c2671616a1f1
dzou <dzou@users.noreply.github.com>
2020-06-02 17:33:37 -0400

    Add Pub/Sub

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,12 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@Autowired
+	PubSubTemplate pubSubTemplate;
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	  pubSubTemplate.publish("exampleTopic", "Hello world.");
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
