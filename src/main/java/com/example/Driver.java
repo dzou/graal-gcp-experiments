65b619af3abcda92286860262120a04a7cd21109
dzou <dzou@users.noreply.github.com>
2020-07-07 12:41:51 -0400

    Client library configs

--- src/main/java/com/example/Driver.java
+++ src/main/java/com/example/Driver.java
@@ -1,63 +1,30 @@
 package com.example;
 
-import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
-import com.google.pubsub.v1.PublishRequest;
-import com.google.pubsub.v1.PublishResponse;
-import com.google.pubsub.v1.PublisherGrpc;
-import com.google.pubsub.v1.PublisherGrpc.PublisherStub;
 import com.google.pubsub.v1.PubsubMessage;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.auth.MoreCallCredentials;
-import io.grpc.stub.StreamObserver;
-import java.io.IOException;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Driver {
 
-  static ManagedChannel channel;
-
-  static PublisherStub publisherStub;
-
-  public static void main(String[] args) throws IOException, InterruptedException {
+  public static void main(String[] args) throws Exception {
     System.out.println("Hello world.");
 
-    channel = ManagedChannelBuilder.forTarget("dns:///pubsub.googleapis.com:443").build();
-    publisherStub = PublisherGrpc.newStub(channel)
-        .withCallCredentials(
-            MoreCallCredentials.from(GoogleCredentials.getApplicationDefault()));
-
-    PublishRequest publishRequest =
-        PublishRequest.newBuilder()
-            .setTopic("projects/my-kubernetes-codelab-217414/topics/exampleTopic")
-            .addMessages(
-                PubsubMessage.newBuilder().setData(ByteString.copyFromUtf8("This was published from the native image.")).build())
+    Publisher publisher =
+        Publisher
+            .newBuilder("projects/my-kubernetes-codelab-217414/topics/exampleTopic")
             .build();
 
-    publisherStub.publish(publishRequest, new UnaryStreamObserver());
+    String message = "HELLO FROM CLIENT LIBS";
+    ByteString data = ByteString.copyFromUtf8(message);
+    PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
 
-    Thread.sleep(5000);
+    publisher.publish(pubsubMessage);
+
+    ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+    String messageId = messageIdFuture.get();
+    System.out.println("Published message ID: " + messageId);
+
+    System.out.println("Good bye world.");
   }
-
-  private static class UnaryStreamObserver implements StreamObserver<PublishResponse> {
-
-    @Override
-    public void onNext(PublishResponse publishResponse) {
-      System.out.println("Publish completed, response received.");
-    }
-
-    @Override
-    public void onError(Throwable throwable) {
-      System.err.println(throwable);
-    }
-
-    @Override
-    public void onCompleted() {
-      System.out.println("Done!!");
-    }
-  }
-
 }
